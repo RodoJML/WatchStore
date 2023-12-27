2a20f4ac1636d58de1826c375251e03d36c5fc09
Rodolfo J. Meneses Leal <rodojml@me.com>
2023-12-27 15:38:34 -0600

    removing unnecesary files

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
-const server = httpm.createServer(
+const server = http.createServer(
     (req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
