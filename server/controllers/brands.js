af0482c4c4e4f8cec37b65948bcd9dc0f573dacc
Rodolfo J. Meneses Leal <rodojml@me.com>
2023-12-28 22:29:35 -0600

    added REST into VS and testing api sql

--- server/controllers/brands.js
+++ server/controllers/brands.js
@@ -3,8 +3,28 @@ const router = express.Router();
 const model = require('../models/brands');
 
 router
-    .get('/', (req, res, next) => {
-        model.getBrands()
+    .get('/getAll', (req, res, next) => {
+        model.getAll()
+        .then(
+            result => {
+                const data = {data: result, total: size, isSuccess: true};
+                res.send(data);
+            }
+        ).catch(next);
+    })
+
+    .get('/getOne/:id', (req, res, next) => {
+        model.getOne(req.params.id)
+        .then(
+            result => {
+                const data = {data: result, total: size, isSuccess: true};
+                res.send(data);
+            }
+        ).catch(next);
+    })
+
+    .post('/addOne', (req, res, next) => {
+        model.addOne(req.body)
         .then(
             result => {
                 const data = {data: result, isSuccess: true};
--- server/models/brands.js
+++ server/models/brands.js
@@ -2,16 +2,99 @@ const { connect } = require('./mysql');
 
 const TABLE_NAME = 'brands';
 
-async function getBrands() {
+// Create a function called getAll() that returns a promise
+// Inside the function:
+// Create a connection to the database by calling connect()
+// Create a query to select all rows from the brands table
+// Execute the query by calling connection.query()
+// Return a promise that resolves with the result of the query
+// Close the connection to the database by calling connection.end()
+// Export the getAll() function
 
+async function getAll() {
     const connection = await connect();
+    const query = `SELECT * FROM ${TABLE_NAME}`;
 
     return new Promise((resolve, reject) => {
-        connection.query(`SELECT * FROM ${TABLE_NAME}`, (err, result) => {
+        connection.query(query, (err, result) => {
             if (err) reject(err);
-            resolve(result);
+            size = result.length;
+            resolve(result, size);
         });
+        connection.end(console.log('Connection closed'));
     });
 }
 
-module.exports = { getBrands }
+// From here on all functions use similar code as getAll()
+
+async function getOne(id) {
+    const connection = await connect();
+    const query = `SELECT * FROM ${TABLE_NAME} WHERE brand_id = ${id}`;
+
+    return new Promise((resolve, reject) => {
+        connection.query(query, (err, result) => {
+            if (err) reject(err);
+            size = result.length;
+            resolve(result, size);
+        });
+        connection.end(console.log('Connection closed'));
+    });
+}
+
+
+async function addOne(name) {
+    const connection = await connect();
+    const query = `INSERT INTO ${TABLE_NAME} (brand_name) VALUES ('${name}')`;
+
+    return new Promise((resolve, reject) => {
+        connection.execute(query, (err, result) => {
+            if (err) reject(err);
+            affectedRows = result.affectedRows;
+            insertedId = result.insertId;
+
+            resolve(result, affectedRows, insertedId);
+        });
+        connection.end(console.log('Connection closed'));
+    });
+}
+
+module.exports = { getAll, getOne, addOne }
+
+
+// The different sql functions are:
+// connection.query() - for queries that return a result set
+// connection.execute() - for queries that do not return a result set
+// connection.beginTransaction() - for transactions
+// connection.commit() - for committing transactions
+// connection.rollback() - for rolling back transactions
+// connection.end() - for closing the connection
+// connection.destroy() - for destroying the connection
+// connection.pause() - for pausing the connection
+// connection.resume() - for resuming the connection
+// connection.escape() - for escaping query values
+// connection.escapeId() - for escaping query identifiers
+// connection.format() - for formatting queries
+// connection.changeUser() - for changing the user
+// connection.ping() - for pinging the server
+// connection.statistics() - for retrieving server statistics
+
+// connection.execute() - for queries that do not return a result set
+    // the return value is an object with the following properties:
+        // affectedRows - the number of affected rows
+        // insertId - the insert id
+        // warningStatus - the warning status
+        // changedRows - the number of changed rows
+        // message - the message
+        // protocol41 - whether the protocol is 4.1
+        // serverStatus - the server status
+        // warningCount - the warning count
+
+
+// To test post using a http file would be:
+// POST http://localhost:3000/brands/addOne
+// Content-Type: application/json
+//
+// {
+//     "brand_name": "Test Brand"
+// }
+// To run this test you need to install the REST Client extension for VS Code.
