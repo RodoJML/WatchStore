17039aa3158a4ffb83f09f7d976d0d02d95d04d4
RodoJML <rodojml@me.com>
2024-01-07 14:17:02 -0600

    removing font awesome added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-
 export default function Navbar() {
     return (
         <>
         <nav className="bg-gray-800 grid grid-cols-3 text-white min-h-14 items-center">
             <div className="col-span-1">
-                <FontAwesomeIcon icon={icon({name: 'user-secret'})} />
             </div>
 
             <div className="col-span-1 text-center">
