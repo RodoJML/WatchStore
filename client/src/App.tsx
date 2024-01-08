2c097e309a996b68524cf9ee67de16de577201dc
RodoJML <rodojml@me.com>
2024-01-07 19:16:48 -0600

    Fontawesome added and navbar first draft

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,18 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+
 export default function Navbar() {
     return (
         <>
-        <nav className="bg-gray-800 grid grid-cols-3 text-white min-h-14 items-center">
-            <div className="col-span-1">
+            <div className="backdrop-blur bg-gray-900">
+                <nav className="grid grid-cols-5 text-white min-h-14 items-center p-4">
+                    <div className="col-span-2 text-left">
+                        <label>WatchStore</label>
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+
+                    <div className="col-span-1 text-center">
+                        <time/>
+                    </div>
+
+                    <div className="col-span-2 text-right">
+                        Wishlist
+                    </div>
+                </nav>
+                
+                <div className="p-4 flex items-center space-x-1">                
+                    <input className="w-full min-h-10 border-gray-500 rounded" type="text" placeholder="Buscar..."></input>
+                    <div className="bg-green-300 text-center p-2 rounded">
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </div>
+                </div>
+                
+
+
             </div>
 
-            <div className="col-span-1 text-center">
-                Test text
-            </div>
 
-            <div className="col-span-1 text-right">
-                User and wishlist
-            </div>
-        </nav>
+            
         </>
     );
 }
