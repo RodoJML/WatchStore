a0a121b0a9e16e472221b9a1362fc2e148755517
RodoJML <rodojml@me.com>
2024-01-08 22:04:49 -0600

    partial implementation of flyout

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,18 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
-// wishlist icon
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
 
 // Dont forget to add stiching to the design
 
 
+
 export default function Navbar() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function Menu({ isMenuOpen }) {
+        return isMenuOpen ? (
+            <div className="bg-white w-40 h-full absolute transition ease-in delay-1000 duration-1000"> My Flyout </div>
+        ) : null;
+    }
+    
+    function toggleMenu() {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
         <>
             <div className="bg-gradient-to-b from-stone-700 to-stone-900">
+
+                <Menu isMenuOpen={isMenuOpen} />
+
                 <nav className="grid grid-cols-5 text-white min-h-14 items-center p-4">
                     <div className="col-span-2 text-left">
-                        <FontAwesomeIcon icon={faBars} />
+                        <FontAwesomeIcon icon={faBars} onClick={toggleMenu}/>
                         <span className="font-bold"> &nbsp; Watch</span><span className="font-light">Store</span>
                     </div>
 
