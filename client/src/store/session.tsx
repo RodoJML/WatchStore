d28c0f250763e5c60b4efc4545ea2ffc54db7618
RodoJML <rodojml@me.com>
2024-01-11 18:23:34 -0600

    Adding session state management through redux

---
+++ client/src/store/session.tsx
@@ -0,0 +1,56 @@
+import * as Fetch from '../model/fetch'
+import { configureStore } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+const defaultSession = {
+    user: null,
+    isLoading: false,
+    messages: [] as { msg: string, type: 'sucess' | 'danger' | 'warning' | 'info' }[],
+    redirectURL: null as string | null,
+}
+
+// When using createSlice from Redux Toolkit, you don't need to write action creators or action types by hand.
+export const session = createSlice({
+    name: 'session',
+    initialState: defaultSession,
+    reducers: {
+        setUser: () => {},
+        setIsLoading: (state, action: PayloadAction<boolean>) => {
+            state.isLoading = action.payload
+        },
+        setMessages: (state, action: PayloadAction<{ msg: string, type: 'sucess' | 'danger' | 'warning' | 'info' }[]>) => {
+            state.messages = action.payload
+        },
+        setRedirectURL: (state, action: PayloadAction<string | null>) => {
+            state.redirectURL = action.payload
+        },
+    }
+})
+
+export const store = configureStore({
+    reducer: {
+        session: session.reducer
+    }
+})
+
+export function api(url: string, data?: any, method?: string, headers?: any) {
+    
+    session.actions.setIsLoading(true)
+
+    return Fetch.api(url, data, method, headers)
+        .catch( err => {
+            console.error({err})
+            session.actions.setMessages([{msg: err.message ?? JSON.stringify(err), type: 'danger'}])
+        })
+        .finally(() => session.actions.setIsLoading(false))
+}
+
+export function useSession() {
+    return session
+}
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const { setUser, setIsLoading, setMessages, setRedirectURL } = session.actions
+export default store
+
