5eb9bd9f888849a81ba9ab8b6c171a571e8b4002
Rodolfo J. Meneses Leal <rodojml@me.com>
2024-01-13 19:38:13 -0600

    unncesary files deleted from model

--- client/src/components/Brands.tsx
+++ client/src/components/Brands.tsx
@@ -1,44 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../state/store/store";
-import { apiFetch } from "../state/store/slice/sessionSlice";
-import { type BrandItem } from "../model/brands";
 import { useEffect, useState } from "react";
-import { setLoading } from "../state/store/slice/sessionSlice";
-import { getAll } from "../state/store/slice/brandsSlice";
+import { getAll, type BrandItem } from "../state/store/slice/brandsSlice";
 import type { DataEnvelopeList, DataEnvelope } from "../model/fetch";
 
 
 export default function Brands(){
 
-    const session = useSelector((state: RootState) => state.session);
+    const sessionState = useSelector((state: RootState) => state.session);
+    const brandsState = useSelector((state: RootState) => state.brands);
     const dispatch = useDispatch<AppDispatch>();
 
     const [brands, setData] = useState<DataEnvelopeList<BrandItem>>();
 
     useEffect(() => {
-        dispatch(setLoading({value: true}));
-
-        dispatch(
-            getAll()).then((res) => {
-                setData(res.payload as DataEnvelopeList<BrandItem>);
-                console.log(brands?.data);
-            }
-        ).finally(() => {
-            dispatch(setLoading({value: false}));
-        })}, []);
+        dispatch(getAll()).then((res) => {
+            setData(res.payload as DataEnvelopeList<BrandItem>)
+        })
+    }, []);
         
     return (
         <>
-            {session.isLoading && <div>Session Loading...</div>}
+            {brandsState.isLoading && <div>Loading...</div>}
 
 
-            {/* <ul>
-                {brands?.items?.data.map((brand) => (
+            <ul>
+                {brands?.data.map((brand) => (
                     <li key={brand.brand_id}>
                         {brand.brand_name}
                     </li>
                 ))}
-            </ul> */}
+            </ul>
         </>
     )
 
