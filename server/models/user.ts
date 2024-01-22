641e8b921bf09b81fb41effc78ac792230a7b4c9
Rodolfo J. Meneses Leal <rodojml@me.com>
2024-01-22 14:31:21 -0600

    Beginning the implementation of JWT

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,1 +1,8 @@
-const Userdatabase = require('./knex.ts');
+const jwt = require('jsonwebtoken');
+
+async function login(email, password) {
+    const reg_email = await database('REG_LOG').select('*').where('reg_email', email);
+    const user_id = reg_email[0].reg_user_id;
+    const user_password = await database('USER').select('user_password').where('user_id', user_id);
+    
+}
