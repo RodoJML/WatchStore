7a8109b5b22c6ae087c3277017a7cd962250ef83
RodoJML <rodojml@me.com>
2024-01-23 22:57:11 -0600

    Add Redux state and dispatch to LoginForm component, connecting backend with frontend

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,28 +1,33 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../state/store/store";
 
-export default function LoginForm(){
+export default function LoginForm() {
 
-    const [loginFormData, setLoginFormData] = useState({email: "",password: "",});
+    const [loginFormData, setLoginFormData] = useState({ email: "", password: "", });
+    const sessionState = useSelector((state: RootState) => state.session);
+    const dispatch = useDispatch<AppDispatch>();
 
-    const handleSubmit = (e: React.SyntheticEvent) =>{
+    const handleSubmit = (e: React.SyntheticEvent) => {
         e.preventDefault();
         // e comes from the event listener, a default behavior of react
         // event listener is the submit button
+
         
     }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         // Handle form field changes and update the state
         const { name, value } = e.target;
-        setLoginFormData({...loginFormData, [name]: value});
-      };
+        setLoginFormData({ ...loginFormData, [name]: value });
+    };
 
     return (
         <div>
             <h1>Login</h1>
 
             <form onSubmit={handleSubmit}>
-                <input type="text" name="email" placeholder="Email" onChange={handleChange}/>
+                <input type="text" name="email" placeholder="Email" onChange={handleChange} />
                 <input type="password" name="password" placeholder="Password" onChange={handleChange} />
                 <button className="bg-amber-400" type="submit">Login</button>
             </form>
