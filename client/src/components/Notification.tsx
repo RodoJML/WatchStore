ab9c868d0083dd2582ae6c4ff7651b8f6dcaa366
RodoJML <rodojml@me.com>
2024-01-29 22:41:41 -0600

    Update Notification component styles

--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -8,7 +8,7 @@ interface NotificationProps {
 export default function Notification({ message }: NotificationProps) {
 
     const [active, setActive] = useState(false);
-    const NotificationStyle = 'grid fixed left-4 right-4 h-20 ease-in-out duration-500 shadow-2xl justify-start items-center rounded-lg bg-white pl-3 text-left';
+    const NotificationStyle = 'grid fixed left-4 right-4 h-20 ease-in-out duration-500 shadow-2xl justify-start items-center rounded-lg bg-white pl-3 text-left bg-white bg-opacity-80';
     const NotificationActive = `${NotificationStyle} top-5 z-20`;
     const NotificationInactive = `${NotificationStyle} -top-20 z-0`;
     // These styles are technically not used but can be useful for future reference
@@ -16,7 +16,7 @@ export default function Notification({ message }: NotificationProps) {
     const errorNotification = `${active ? NotificationActive : NotificationInactive} border-red-500 border-opacity-20 bg-red-500`;
 
     useEffect(() => {
-        if (message.message !== null){
+        if (message.message !== null) {
             setActive(true);
             setTimeout(() => setActive(false), 5000);
         }
@@ -25,29 +25,13 @@ export default function Notification({ message }: NotificationProps) {
     return <div className={`${message.type === 'success' ? successNotification : errorNotification}`}>
         <div className="text-stone-800 text-sm">
             {message.type === 'success' &&
-                <div>
-                    <span>✅ </span>
-                    <span>Tipo: </span>
-                    <span className="text-green-800 capitalize">{message.type}</span>
-                </div>
-            }
+                <span className="text-green-800 capitalize">✅ {message.type}</span>}
             {message.type === 'danger' &&
-                <div>
-                    <span>🚨 </span>
-                    <span>Tipo: </span>
-                    <span className="text-red-800 capitalize">{message.type}</span>
-                </div>
-            }
+                <span className="text-red-800 capitalize">🚨 {message.type}</span>}
             {message.type === 'warning' &&
-                <div>
-                    <span>🚸 </span>
-                    <span>Tipo: </span>
-                    <span className="text-yellow-800 capitalize">{message.type}</span>
-                </div>
-            }
+                <span className="text-yellow-800 capitalize">🚸 {message.type}</span>}
             {message.type === 'info' &&
-                <span>ℹ️ Tipo: {message.type}</span>
-            }
+                <span>ℹ️ {message.type}</span>}
         </div>
         <div className="absolute bg-black bg-opacity-10 h-px w-11/12 place-self-center"></div>
         <div className="text-stone-800 text-sm whitespace-nowrap text-nowrap overflow-scroll">
